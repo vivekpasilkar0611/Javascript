0c2f9348435857f67248b10d8114e4d8a5c12841
vivekpasilkar0611 <154726285+vivekpasilkar0611@users.noreply.github.com>
2024-01-10 19:07:40 +0000

    save

---
+++ 01-Basics/Variable.js
@@ -0,0 +1,10 @@
+const accountId = 9067772641
+let accountEmail = "vivekpasilkar0611@gmail.com"
+var accountPassword = "9067772641"
+accountCity = "Vadodara"
+
+// accountId = 1 //
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
